1fa3af48fa9f745cbe5a09cde88f6bb6e559ea14
osama-shawir <89782802+osama-shawir@users.noreply.github.com>
2023-09-27 18:37:03 +0000

    run3

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
-"""
-ETL-Query script
-"""
+# """
+# ETL-Query script
+# """
 
+# from mylib.extract import extract
+# from mylib.transform_load import load
+# from mylib.query import query
+
+# # Extract
+# print("Extracting data...")
+# extract()
+
+# # Transform and load
+# print("Transforming data...")
+# load()
+
+# # Query
+# print("Querying data...")
+# query()
+
+# """
+# ETL-Query script
+# """
+
+import argparse
 from mylib.extract import extract
 from mylib.transform_load import load
 from mylib.query import query
 
-# Extract
-print("Extracting data...")
-extract()
 
-# Transform and load
-print("Transforming data...")
-load()
+def main(args):
+    # Extract
+    print("Extracting data...")
+    extract()
 
-# Query
-print("Querying data...")
-query()
+    # Transform and load
+    print("Transforming data...")
+    load()
+
+    # Query
+    if args.query:
+        print("Querying data...")
+        query()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="ETL-Query script")
+    parser.add_argument("--query", action="store_true", help="query the database")
+    args = parser.parse_args()
+    main(args)
