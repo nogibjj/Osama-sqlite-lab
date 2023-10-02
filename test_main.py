95956059cf6594bfc547cc7b214df1ca38babe2b
osama-shawir <89782802+osama-shawir@users.noreply.github.com>
2023-10-02 00:20:42 +0000

    join

--- test_main.py
+++ test_main.py
@@ -1,10 +1,23 @@
-"""
-Test goes here
 
-"""
+import main
+import subprocess
 
-from mylib.calculator import add
+def test_main():
+    """
+    This functions tests
+    our main function"""
+# Run the script with the flag
+    result = subprocess.run(['python', 
+                             'main.py', '--query'], stdout=subprocess.PIPE)
+    output = result.stdout.decode('utf-8')
+    with open('output.txt', 'w') as f:
+        f.write(output)
 
+# Add, commit, and push the file to your GitHub repository
+    subprocess.run(['git', 'add', 'output.txt'])
+    subprocess.run(['git', 'commit', '-m', 'Added output.txt'])
+    subprocess.run(['git', 'push'])
 
-def test_add():
-    assert add(1, 2) == 3
+if __name__ == '__main__':
+    test_main()
+
